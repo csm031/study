8a870598ea36c5a6d1f441f5072aad8260651f98
csm031 <csm031@naver.com>
2024-03-07 16:49:58 +0900

    24.03.07 commit

--- TEST.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    let createButton = document.createElement("button");
-    let buttonText = document.createTextNode('Click');
-    createButton.innerHTML = `버튼1`;
-    createButton.appendChild(buttonText);
-    document.body.appendChild(createButton);
-    document.body.ap
-}
