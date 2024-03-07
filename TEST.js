8f846771dd0886b7038171d140e73688b1bdd0d0
csm031 <csm031@naver.com>
2024-03-07 15:17:51 +0900

    24.03.07 commit

--- TEST.js
+++ TEST.js
@@ -1,13 +1,8 @@
-function doNTimes (func, repeat, x, y) {
-    let result = x;
-    for (i = 0; i< repeat; i++) {
-        result = func(result, y);
-    }
-    return result;
+window.onload = () => {
+    let createButton = document.createElement("button");
+    let buttonText = document.createTextNode('Click');
+    createButton.innerHTML = `버튼1`;
+    createButton.appendChild(buttonText);
+    document.body.appendChild(createButton);
+    document.body.ap
 }
-
-console.log (
-    doNTimes((x,y) => x * y , 3, 5,2),//40
-    doNTimes((x,y) => x / y , 3, 5,2)
-)
-
