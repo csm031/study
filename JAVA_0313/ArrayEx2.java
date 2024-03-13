e42d38d509453adfd88b71e37f69679db7b3d9dd
csm031 <csm031@naver.com>
2024-03-13 16:40:14 +0900

    03.13 commit

---
+++ JAVA_0313/ArrayEx2.java
@@ -0,0 +1,33 @@
+package JAVA_0313;
+import java.sql.Array;
+import java.util.Scanner;
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int [] numbers = new int[5];
+        System.out.println("5개의 정수를 입력하세요: ");
+        for (int i = 0; i <5 ; i++) {
+            numbers[i] = scan.nextInt();
+        }
+        System.out.println("출력");
+        for (int i = 0; i <5 ; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length-1) {
+                System.out.print(", ");
+            }
+        }
+        //입력받은 역순으로 출력
+        System.out.println("역순으로 출력합니다");
+        System.out.println("5개의 정수를 입력하세요: ");
+        for (int i = 4; i >=0 ; i--) {
+            numbers[i] = scan.nextInt();
+        }
+        System.out.println("출력");
+        for (int i = 0; i <5 ; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length-1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
