c5c58a7f1eb10ba617b6c6711076c6239258950f
csm031 <csm031@naver.com>
2024-03-14 11:12:44 +0900

    03.14 commit

--- JAVA_0314/MethodEx2.java
+++ JAVA_0314/MethodEx2.java
@@ -1,5 +1,15 @@
 package JAVA_0314;
 
 public class MethodEx2 {
+    public static void main(String[] args) {
+        outPut("Hello, world!",3);
+        outPut("Hello, world!",5);
+        outPut("Hello, world!",7);
+    }
 
+    public static void outPut(String word, int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.println(word);
+        }
+    }
 }
