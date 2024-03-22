1d51e2e2c84ebecf9dbad0c58a20b801a4f75df6
csm031 <csm031@naver.com>
2024-03-22 12:32:58 +0900

    03.22 생성자

---
+++ Inflearn/JAVA_0322/ConstructMain1.java
@@ -0,0 +1,15 @@
+package Inflearn.JAVA_0322;
+
+public class ConstructMain1 {
+
+    public static void main(String[] args) {
+        MemberConstruct member1 = new MemberConstruct("user1", 15, 90);
+        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+
+        MemberConstruct[] memberConstructs = {member1, member2};
+
+        for (MemberConstruct member : memberConstructs) {
+            System.out.println("이름: " + member.name + " 나이 : " + member.age + " 성적 : " + member.grade);
+        }
+    }
+}
