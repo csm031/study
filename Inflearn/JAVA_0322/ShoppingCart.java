c0f0d99af770b4bda10d18e299a65dd0676fca1a
csm031 <csm031@naver.com>
2024-03-22 17:28:19 +0900

    03.22 접근 제어자 문제2 임시

---
+++ Inflearn/JAVA_0322/ShoppingCart.java
@@ -0,0 +1,10 @@
+package Inflearn.JAVA_0322;
+
+public class ShoppingCart {
+    private Item[] items = new Item[10];
+    private int itemCount;
+
+    public void addItem(Item item) {
+        if (items.length<)
+    }
+}
