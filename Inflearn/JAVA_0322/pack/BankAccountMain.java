6a592955b6b708eacfd09f581c54c3d1c0b0df48
csm031 <csm031@naver.com>
2024-03-22 17:01:28 +0900

    03.22 접근 제어자

---
+++ Inflearn/JAVA_0322/pack/BankAccountMain.java
@@ -0,0 +1,10 @@
+package Inflearn.JAVA_0322.pack;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(10000);
+        account.withdraw(3000);
+        System.out.println("balance = " + account.getBalance());
+    }
+}
