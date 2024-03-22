612626657a40f52752bdfb5a592e3702a37f4cf9
csm031 <csm031@naver.com>
2024-03-22 09:48:16 +0900

    03.22 절차지향 프로그래밍 문제풀이

---
+++ Inflearn/JAVA_0322/MusicPlayerData.java
@@ -0,0 +1,6 @@
+package Inflearn.JAVA_0322;
+
+public class MusicPlayerData {
+    int volume = 0;
+    boolean isOn = false;
+}
---
+++ Inflearn/JAVA_0322/ValueData.java
@@ -0,0 +1,10 @@
+package Inflearn.JAVA_0322;
+
+public class ValueData {
+    int value;
+
+    void add() {
+        value++;
+        System.out.println("숫자 증가 = " + value);
+    }
+}
