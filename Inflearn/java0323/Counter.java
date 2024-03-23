8d79141a6a0ec5077607d1af76ba3abc66ad2e39
csm031 <csm031@naver.com>
2024-03-23 22:18:29 +0900

    03.23 static 1

---
+++ Inflearn/java0323/Counter.java
@@ -0,0 +1,5 @@
+package Inflearn.java0323;
+
+public class Counter {
+    public int count;
+}
---
+++ Inflearn/java0323/Data2.java
@@ -0,0 +1,10 @@
+package Inflearn.java0323;
+
+public class Data2 {
+    public String name;
+
+    public Data2(String name, Counter counter) {
+        this.name = name;
+        counter.count++;
+    }
+}
