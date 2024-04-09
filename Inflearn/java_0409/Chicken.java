c90defc3b2afd18423c890f47b0c4af6ace6e7c2
csm031 <csm031@naver.com>
2024-04-09 17:25:05 +0900

    04 09 클래스와 인터페이스

---
+++ Inflearn/java_0409/Chicken.java
@@ -0,0 +1,13 @@
+package Inflearn.java_0409;
+
+public class Chicken  extends AbstractAnimal implements Fly{
+    @Override
+    public void sound() {
+        System.out.println("꼬끼오");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("닭 날기");
+    }
+}
