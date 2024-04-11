9aac8d405c7c0457443694e6bc321ae395ff94a7
csm031 <csm031@naver.com>
2024-04-11 17:38:58 +0900

    04 11 다형성 역할 구현예제 1

---
+++ Inflearn/java_0411/car/K3Car.java
@@ -0,0 +1,16 @@
+package Inflearn.java_0411.car;
+
+public class K3Car {
+
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void pressAccelerator() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+}
